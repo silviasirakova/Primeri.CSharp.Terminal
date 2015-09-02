66e5f3255def3efed4af2e9b4c07fea546022802
Силвия Сиракова <sbsirakova@abv.bg>
2015-09-02 18:35:16 +0300

    Modul Модул 02, Видео 05 - Четене от ред

---
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReadFromCommandLine
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниране на променливи
+			int a = 5, b = 0;
+
+			//Въвеждане на входни параметри
+			Console.Write( "Моля въведете а: " );
+			b = Convert.ToInt32 ( Console.ReadLine () );
+
+			//Печат на резултат
+			Console.WriteLine ( "Резултата е a + b " + (a + b).ToString () + "\n\n\n");
+		}
+	}
+}
